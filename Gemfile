cdf99a32e6c1eb18b642d379fdd0da2a9eeb6f2e
puping <pingbro@163.com>
2018-05-02 16:34:47 +0800

    升级rails 5.0.7->5.2.0

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://gems.ruby-china.org'
 
-gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
+gem 'rails', '~> 5.2'
 gem 'mysql2', '>= 0.3.18', '< 0.5'
+gem 'bootsnap', '~> 1.3'
 
 gem 'puma', '~> 3.0'
 
--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,17 @@
 require_relative 'boot'
+
 require 'rails/all'
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Myblog
   class Application < Rails::Application
-    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+
     config.i18n.default_locale = 'zh'
 
     config.autoload_paths += %W(#{config.root}/lib)
@@ -27,6 +33,5 @@ module Myblog
         request_specs: false
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
-
   end
 end
