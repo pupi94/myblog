27a403cbe00c2a7a49e102587385aef5b6bffdf2
puping <pingbro@163.com>
2018-05-14 12:17:13 +0800

    文章显示ui优化；markdown 文本转换html功能优化

--- config/initializers/myblog_init.rb
+++ config/initializers/myblog_init.rb
@@ -6,4 +6,12 @@ Log = Rails.logger
 Log.level = Rails.logger.level
 
 # 相关参数文档 http://www.rubydoc.info/gems/redcarpet/3.4.0
-BlogMarkdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+BlogMarkdown = Redcarpet::Markdown.new(
+  Redcarpet::Render::HTML,
+  fenced_code_blocks: true,
+  disable_indented_code_blocks: true,
+  strikethrough: true,#解析删除线
+  lax_spacing:true,
+  space_after_headers:true,
+  superscript:true
+)
