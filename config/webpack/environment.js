139e433ae72bb29cec645f9f4e16b21f7260a7e4
huang puping <huangpuping@shoplazza.com>
2019-09-21 17:39:09 +0800

    add react package

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,20 +1,3 @@
 const { environment } = require('@rails/webpacker');
 
-const webpack = require('webpack');
-  environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-}));
-
-environment.loaders.append('expose', {
-  test: require.resolve('jquery'),
-  use: [{
-    loader: 'expose-loader',
-    options: '$'
-  }, {
-    loader: 'expose-loader',
-    options: 'jQuery'
-  }]
-});
 module.exports = environment;
